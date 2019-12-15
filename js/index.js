67fdecd33d2e1f8364c5a562919eb943131d5c00
khanajaz96 <52720034+khanajaz96@users.noreply.github.com>
2019-12-15 14:31:01 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
+// NAVBAR
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
+
 // COUNTER
-// Counter
 $('.counter').counterUp({
   delay: 10,
-  time: 2000
+  time: 2000,
 });
 $('.counter').addClass('animated fadeInDownBig');
 $('h3').addClass('animated fadeIn');
-// Testimonials
+// hr
